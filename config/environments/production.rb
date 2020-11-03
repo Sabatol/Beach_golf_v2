879d42072d7a43ef64edeab640b0d2bb32d2c31f
Sabatol <u.sacrez@gmail.com>
2020-11-03 14:14:50 +0100

    Update Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,6 @@ Rails.application.configure do
 
     #config.action_mailer.delivery_method = :letter_opener
     config.action_mailer.perform_deliveries = true
+
+    config.action_mailer.default_url_options = { :host => 'beach-golf.herokuapp.com' }
 end
