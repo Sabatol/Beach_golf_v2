f8210065855e1c8b29cc67a23d9ef04f65d39445
Sabatol <u.sacrez@gmail.com>
2020-11-03 10:18:55 +0100

    Update mailer

--- app/mailers/participation_mailer.rb
+++ app/mailers/participation_mailer.rb
@@ -1,14 +1,14 @@
 class ParticipationMailer < ApplicationMailer
   default from: 'machin@yopmail.com'
  
-  def welcome_email(user)
+  def participation_email(participation)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
-
+    @participation = participation
+    @user = @participation.user 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez Beach-Golf-France !') 
+    mail(to: @participation.event.user.email, subject: 'Bienvenue chez Beach-Golf-France !') 
   end
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,8 +2,8 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
+  def participation_send
+    UserMailer.participation_email(self).deliver_now
   end
 
 end
