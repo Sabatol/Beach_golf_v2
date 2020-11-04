5ada483f66760966ccf3c8d2c4b187dfa00b52d9
Sabatol <u.sacrez@gmail.com>
2020-11-04 15:51:21 +0100

    Update Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'events#index'
   resources :users, only: [:show, :edit, :update]
-  resources :events
-  resources :participations
+  resources :events do
+    resources :charges
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
